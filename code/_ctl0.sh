d2795187c9617e9250540623baeb86b42b20e604
Nhan Tran <thcnhan@gmail.com>
2017-04-17 19:00:57 +0800

    Finished model: 1 process runs, done, kill

---
+++ code/_ctl0.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#set -e
+
+# Worker Intro
+echo
+echo "<<<<<-----"
+echo "$(date) Enter ${0}, pid: $(cat ${0}.pid)"
+
+# Worker Variables
+DL="/home/cnhan21/dl/"
+BRATS=$DL"BRATS2015/"
+TUMOR_CROPPED=$BRATS"tumor_cropped/"
+
+TF=$DL"bin/activate"
+
+# Worker Body
+echo "WORKER: ${0##*/}"
+echo "-----All bashes by $(whoami)-----"
+ps -fu $(whoami) | grep bash
+
+echo "-----Bash of current control-----"
+#ps -fu $(whoami) | grep -P "$(cat ${0}.pid)\s*1"
+#[not-used]ps -opid= | grep -P "^\s*$(cat ${0}.pid)$"
+
+echo "-----Processes relating to Bash of current control-----"
+#ps -fu $(whoami) | grep -P "$(cat ${0}.pid)"
+
+#echo "-----Listing files in ${TUMOR_CROPPED}-----"
+
+echo "-----Load TensorFlow-----"
+source ${TF}
+
+echo "-----Check Tensorflow version-----"
+#python -c 'import tensorflow as tf; print(tf.__version__)' # for Python 2
+#pip list --format=columns| grep tensorflow
+
+#echo "-----List cron jobs of $(whoami)-----"
+#crontab -u $(whoami) -l
+
+echo "-----Run-----"
+
+
+# Worker End
+if [ -f ${0}.pid ]; then
+  echo "$(date) Exit ${0}, pid: $(cat ${0}.pid)"
+  rm -r ${0}.pid
+fi
+echo "----->>>>>"
--- code/controller.sh
+++ code/controller.sh
@@ -2,50 +2,93 @@
 #set -e
 
 CTL="/home/cnhan21/Dropbox/cj/ctl"
+SCD_NAME=${0##*/}
+CSD_LOG="/home/cnhan21/Dropbox/cj/controller.log"
+MX_LOG_SZ_CTL=3000 # CONTROLLER
+MX_LOG_SZ_SCD=3000 # SCHEDULER
+
+# Limit log size
+LOG_SZ_SCD=$(stat -c%s $CSD_LOG)
+if [ "$LOG_SZ_SCD" -gt "$MX_LOG_SZ_SCD" ]; then
+  rm $CSD_LOG
+fi
 
 # Control Intro
 echo
 echo "<<<<<-----"
 echo "$(date) Enter ${0}, pid: $(cat ${0}.pid)"
 
-for CTL_SCRIPT in ${CTL}/*.sh
-do
-  echo
-  echo "$(date) Start iteration: ${CTL_SCRIPT}"
+if [ -e ${0}.pid_old ]; then
+  if (ps -u $(whoami) -opid= | grep -P "^\s*$(cat ${0}.pid_old)$" &> /dev/null); then
+    echo "-----Old ${SCD_NAME} is running on-----"
+    ps -fu $(whoami) | grep -P "$(cat ${0}.pid_old)"
 
-  if [ -f ${CTL_SCRIPT}.done ]; then
-    echo "${CTL_SCRIPT}.done exists. Skip to the next script."
-    continue
-  fi
+    if [ -e ${0}.kill ]; then
+      echo "Attempt to kill old ${SCD_NAME}"
 
-  if [ -f ${CTL_SCRIPT}.kill ]; then
-    echo "Attempt to kill ${CTL_SCRIPT}"
-    if $(kill $(cat ${CTL_SCRIPT}.pid)); then
-      echo "Successfully killed ${CTL_SCRIPT}"
-      if [ -f ${CTL_SCRIPT}.pid ]; then
-        rm -v ${CTL_SCRIPT}.pid
+      if $(kill $(cat ${0}.pid_old)); then
+        echo "Successfully killed old ${SCD_NAME}"
+      else
+        echo "Failed to kill old ${SCD_NAME}"
       fi
-    else
-      echo "Failed to kill ${CTL_SCRIPT}"
     fi
-    continue
+  elif [ -e ${0}.done ]; then
+    echo "${SCD_NAME} was done. Exiting."
+  elif [ -e ${0}.kill ]; then
+    echo "${SCD_NAME} is on kill. Exiting."
+  else
+    for CTL_SCRIPT in ${CTL}/*.sh; do
+      CTL_NAME=${CTL_SCRIPT##*/}
+      echo
+      echo "$(date) Start iteration: ${CTL_NAME}"
+
+      if (ps -u $(whoami) -opid= | grep -P "^\s*$(cat ${CTL_SCRIPT}.pid)$" &> /dev/null); then
+        echo "-----${CTL_NAME} is running on-----"
+        ps -fu $(whoami) | grep -P "$(cat ${CTL_SCRIPT}.pid)"
+
+        if [ -e ${CTL_SCRIPT}.kill ]; then
+          echo "Attempt to kill ${CTL_NAME}"
+
+          if $(kill $(cat ${CTL_SCRIPT}.pid)); then
+            echo "Successfully killed ${CTL_NAME}"
+            if [ -e ${CTL_SCRIPT}.pid ]; then
+              rm -v ${CTL_SCRIPT}.pid
+            fi
+          else
+            echo "Failed to kill ${CTL_NAME}"
+          fi
+        fi
+      elif [ -e ${CTL_SCRIPT}.done ]; then
+        echo "${CTL_NAME} was done. Skip to the next script."
+        continue
+      elif [ -e ${CTL_SCRIPT}.kill ]; then
+        echo "${CTL_NAME} is on kill. Skip to the next script."
+        continue
+      else
+        LOG_SZ_CTL=$(stat -c%s ${CTL_SCRIPT}.log)
+        if [ "$LOG_SZ_CTL" -gt "$MX_LOG_SZ_CTL" ]; then
+          bash ${CTL_SCRIPT} > ${CTL_SCRIPT}.log 2>&1 & echo $! > ${CTL_SCRIPT}.pid
+        else
+          bash ${CTL_SCRIPT} >> ${CTL_SCRIPT}.log 2>&1 & echo $! > ${CTL_SCRIPT}.pid
+        fi
+      fi
+      
+      echo "$(date) End iteration: ${CTL_SCRIPT}"
+    done
+
+    if [ -e ${0}.pid ]; then
+      mv -v ${0}.pid ${0}.pid_old
+    fi
   fi
-
-  if [ -f ${CTL_SCRIPT}.pid ]; then
-    echo "${CTL_SCRIPT} is being run"
-    continue
-  fi
-
-  bash ${CTL_SCRIPT} >> ${CTL_SCRIPT}.log 2>&1 & echo $! > ${CTL_SCRIPT}.pid
-
-  echo
-  echo "$(date) End iteration: ${CTL_SCRIPT}"
-done
+else
+  mv -v ${0}.pid ${0}.pid_old
+fi
 
 # Control End
 if [ -f ${0}.pid ]; then
   echo "$(date) Exit ${0}, pid: $(cat ${0}.pid)"
-  rm -r ${0}.pid
+elif [ -f ${0}.pid_old ]; then
+  echo "$(date) Exit ${0}, pid: $(cat ${0}.pid_old)"
 fi
 echo "----->>>>>"
 # echo "# arguments called with ----> ${@}    "
@@ -53,5 +96,3 @@ echo "----->>>>>"
 # echo "# \$2 ----------------------> $2      "
 # echo "# path to me ---------------> ${0}    "
 # echo "# parent path --------------> ${0%/*} "
-# echo "# my name ------------------> ${0##*/}"
-
